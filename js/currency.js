4fc98a4a8d0edff1084704bba189aa3cc99bca29
Fahim Nizamee <nizameef@gmail.com>
2022-06-05 21:26:47 +0600

    up

--- js/currency.js
+++ js/currency.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 }
                 if(amount == 0 )
                 {
-                    document.querySelector('.display-result').innerHTML = `Please Enter an Amount`;
+                    document.querySelector('.display-result').innerHTML = `$ Please Enter an Amount $`;
                     return false
                 }
                 document.querySelector('.display-result').innerHTML = `${amount} ${base.toUpperCase()} equal to ${currencyTo} ${convert().toFixed(2)}`;
