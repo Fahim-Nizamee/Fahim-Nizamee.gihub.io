7054881357002a783567d5755dcb3afbb0e72747
Fahim Nizamee <nizameef@gmail.com>
2022-06-09 02:41:35 +0600

    up

---
+++ js/unit-convert.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#continue").click(function() {
+      var num1 = $("#unitselect").val();
+
+      const element = document.getElementById("change");
+    element.remove();
+
+    });
+  });
